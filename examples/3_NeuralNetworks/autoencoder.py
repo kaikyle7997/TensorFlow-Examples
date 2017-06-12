9566dcdac2fd190ea061756a5aa1bbe950cba11a
Kai Jiau (MacPro) <kasimjm7997@gmail.com>
2017-06-12 18:38:54 +0800

    add tensorboard logs

--- examples/3_NeuralNetworks/autoencoder.py
+++ examples/3_NeuralNetworks/autoencoder.py
@@ -32,41 +32,71 @@ n_hidden_2 = 128 # 2nd layer num features
 n_input = 784 # MNIST data input (img shape: 28*28)
 
 # tf Graph input (only pictures)
-X = tf.placeholder("float", [None, n_input])
+with tf.name_scope('inputs') as scope:
+    X = tf.placeholder("float", [None, n_input], name='input_X')
 
-weights = {
-    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
-    'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
-}
-biases = {
-    'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'decoder_b2': tf.Variable(tf.random_normal([n_input])),
-}
+# weights = {
+#     'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+#     'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+#     'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
+#     'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
+# }
+# biases = {
+#     'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+#     'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
+#     'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+#     'decoder_b2': tf.Variable(tf.random_normal([n_input])),
+# }
+
+
+_weights, _biases = {}, {}
+
+with tf.name_scope('encoder_h1') as scope:
+    _weights['encoder_h1'] = tf.Variable(tf.random_normal([n_input, n_hidden_1]),
+    name='en_h1_weights')
+    _biases['encoder_b1'] = tf.Variable(tf.random_normal([n_hidden_1]),
+    name='en_h1_biases')
+
+with tf.name_scope('encoder_h2') as scope:
+    _weights['encoder_h2'] = tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]),
+    name='en_h2_weights')
+    _biases['encoder_b2'] = tf.Variable(tf.random_normal([n_hidden_2]),
+    name='en_h2_biases')
+
+with tf.name_scope('decoder_h1') as scope:
+    _weights['decoder_h1'] = tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1]),
+    name='de_h1_weights')
+    _biases['decoder_b1'] = tf.Variable(tf.random_normal([n_hidden_1]),
+    name='de_h1_biases')
+
+with tf.name_scope('decoder_h2') as scope:
+    _weights['decoder_h2'] = tf.Variable(tf.random_normal([n_hidden_1, n_input]),
+    name='de_h2_weights')
+    _biases['decoder_b2'] = tf.Variable(tf.random_normal([n_input]),
+    name='de_h2_biases')
 
 
 # Building the encoder
 def encoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
-                                   biases['encoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
-                                   biases['encoder_b2']))
+    with tf.name_scope('encoder') as scope:
+        # Encoder Hidden layer with sigmoid activation #1
+        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, _weights['encoder_h1']),
+                                    _biases['encoder_b1']))
+        # Decoder Hidden layer with sigmoid activation #2
+        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, _weights['encoder_h2']),
+                                    _biases['encoder_b2']))
     return layer_2
 
 
 # Building the decoder
 def decoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
-                                   biases['decoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
-                                   biases['decoder_b2']))
+    with tf.name_scope('decoder') as scope:
+        # Encoder Hidden layer with sigmoid activation #1
+        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, _weights['decoder_h1']),
+                                    _biases['decoder_b1']))
+        # Decoder Hidden layer with sigmoid activation #2
+        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, _weights['decoder_h2']),
+                                    _biases['decoder_b2']))
     return layer_2
 
 # Construct model
@@ -79,14 +109,18 @@ y_pred = decoder_op
 y_true = X
 
 # Define loss and optimizer, minimize the squared error
-cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+with tf.name_scope('squ_error') as scope:
+    cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+with tf.name_scope('optimizer') as scope:
+    optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
 
 # Launch the graph
 with tf.Session() as sess:
+    # writer = tf.train.SummaryWriter("logs/", sess.graph)
+    writer = tf.summary.FileWriter('logs/',sess.graph)
     sess.run(init)
     total_batch = int(mnist.train.num_examples/batch_size)
     # Training cycle
